e77b6426bdab020c198d0f74eaa9de07044e6db9
Lukas Wunner <lukas@wunner.de>
2022-02-24 05:40:33 +0100

    printk: Unregister boot consoles on register of 1st real console
    The code comment preceding register_console() claims that:
    
       "There are two types of consoles - bootconsoles (early_printk) and
        "real" consoles (everything which is not a bootconsole) which are
        handled differently. [...]
        As soon as a "real" console is registered, all bootconsoles
        will be unregistered automatically."
    
    But that's not what the code does:  The code unregisters bootconsoles
    only when the *preferred* console registers, i.e. the last one on the
    command line.  If that console's driver never registers (e.g. because
    it is disabled in the kernel config), bootconsoles stay around
    indefinitely.  Should the command line contain both a bootconsole as
    well as a real console on the same serial port, all messages are logged
    twice once the real console registers.
    
    Moreover, the log buffer is replayed once the real console registers
    even though the messages were already emitted by the bootconsole.
    
    Amend the code to be congruent with the above-quoted code comment and
    thereby avoid these issues.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    https://lore.kernel.org/all/13e4bdf7677924c689a70d0b7ad970a3255a8d41.1677213245.git.lukas@wunner.de
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -3785,10 +3785,8 @@ void register_console(struct console *newcon)
 	 * the real console are the same physical device, it's annoying to
 	 * see the beginning boot messages twice
 	 */
-	if (bootcon_registered &&
-	    ((newcon->flags & (CON_CONSDEV | CON_BOOT)) == CON_CONSDEV)) {
+	if (bootcon_registered && !(newcon->flags & CON_BOOT))
 		newcon->flags &= ~CON_PRINTBUFFER;
-	}
 
 	newcon->dropped = 0;
 	console_init_seq(newcon, bootcon_registered);
@@ -3838,8 +3836,7 @@ void register_console(struct console *newcon)
 	 * went to the bootconsole (that they do not see on the real console)
 	 */
 	con_printk(KERN_INFO, newcon, "enabled\n");
-	if (bootcon_registered &&
-	    ((newcon->flags & (CON_CONSDEV | CON_BOOT)) == CON_CONSDEV) &&
+	if (bootcon_registered && !(newcon->flags & CON_BOOT) &&
 	    !keep_bootcon) {
 		struct hlist_node *tmp;
 
