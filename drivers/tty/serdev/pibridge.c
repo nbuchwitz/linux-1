5ef290c1380980aa2ddf5b97628d849136c9a4ac
Lino Sanfilippo <l.sanfilippo@kunbus.com>
2022-07-15 19:14:36 +0200

    pibridge-serdev: do not wait for response for bc gateway frames
    There is no response for broadcast gateway frames. So in this case return
    immediately in pibridge_req_gate_tmt().
    
    Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

--- drivers/tty/serdev/pibridge.c
+++ drivers/tty/serdev/pibridge.c
@@ -8,6 +8,7 @@
 
 #define REV_PI_IO_TIMEOUT           10         // msec
 #define REV_PI_RECV_BUFFER_SIZE     100
+#define REV_PI_BC_ADDR		    0xff
 
 #define pb_err(n, fmt, ...) \
 	do {\
@@ -259,6 +260,9 @@ int pibridge_req_gate_tmt(u8 dst, u16 cmd, u8 *snd_buf, u16 snd_len,
 				dst, cmd, snd_len);
 		return -EIO;
 	}
+	/* Do not wait for a response in case of a broadcast address */
+	if (dst == REV_PI_BC_ADDR)
+		return 0;
 
 	if (sizeof(pkthdr) != pibridge_recv_timeout((u8 *)&pkthdr, sizeof(pkthdr), tmt)){
 		pb_err(25, "receive head error in gate-req(hdr_len:%d, timeout:%d, data0:%c)\n",
