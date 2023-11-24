980172cb2c18230abb23f95bb0b326a5527455e8
Lino Sanfilippo <l.sanfilippo@kunbus.com>
2023-11-24 13:03:23 +0100

    pibridge-serdev: change datatype for data length in pibridge_send()
    Allow to send more than 255 byte at once with pibridge_send(). This is
    needed for larger frames e.g. when updating the firmware.
    
    Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

--- include/linux/pibridge_comm.h
+++ include/linux/pibridge_comm.h
@@ -13,6 +13,6 @@ int pibridge_req_gate_tmt(u8 dst, u16 cmd, void *snd_buf, u8 snd_len,
 int pibridge_req_send_gate(u8 dst, u16 cmd, void *snd_buf, u8 snd_len);
 int pibridge_recv(void *buf, u8 len);
 int pibridge_recv_timeout(void *buf, u8 len, u16 timeout);
-int pibridge_send(void *buf, u8 len);
+int pibridge_send(void *buf, u32 len);
 void pibridge_clear_fifo(void);
 #endif	/* _PIBRIDGE_COMM_H */
